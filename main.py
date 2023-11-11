fa34aa35d5ecb3ff0e5b357aecacf105af5eea90
KuoHaoZeng <khzeng@cs.washington.edu>
2023-11-11 15:46:59 -0800

    update

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ except:
 def load_dataset() -> prior.DatasetDict:
     """Load the houses dataset."""
     data = {}
-    for split, size in zip(("train", "val"), (94559, 714)):
+    for split, size in zip(("train", "val"), (94310, 2711)):
         if split == "train":
-            d = "./simple_explore_house_v0.13_train.jsonl.gz"
+            d = "./SimpleExploreHouse_train.jsonl.gz"
         else:
-            d = "./explore_house_v0.8_val.jsonl.gz"
+            d = "./SimpleExploreHouse_val.jsonl.gz"
         with gzip.open(d, "r") as f:
             houses = [line for line in tqdm(f, total=size, desc=f"Loading {split}")]
         data[split] = LazyJsonDataset(data=houses, dataset="explore_house", split=split)
